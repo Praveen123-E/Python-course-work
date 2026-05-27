eb4ab39000849e4d9c192d9b222adf49b0c67dad
Praveen123-E <elkapellypraveen@gmail.com>
2026-05-27 17:51:00 +0530

    Add files via upload

---
+++ Day2/result.py
@@ -0,0 +1,27 @@
+Python 3.13.5 (tags/v3.13.5:6cb20a2, Jun 11 2025, 16:15:46) [MSC v.1943 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> 
+= RESTART: C:/Users/praveen kumar/OneDrive/Desktop/Python-course-work/Day2/keyword.py
+['False', 'None', 'True', 'and', 'as', 'assert', 'async', 'await', 'break', 'class', 'continue', 'def', 'del', 'elif', 'else', 'except', 'finally', 'for', 'from', 'global', 'if', 'import', 'in', 'is', 'lambda', 'nonlocal', 'not', 'or', 'pass', 'raise', 'return', 'try', 'while', 'with', 'yield']
+35
+>>> Myvar=10
+>>> myvar1=20
+>>> myvar@1=30
+SyntaxError: cannot assign to expression here. Maybe you meant '==' instead of '='?
+>>> my_var=10
+>>> myvar var=10
+SyntaxError: invalid syntax
+>>> my_var
+10
+>>> myvar1
+20
+>>> _aa=10
+>>> _bb=34
+>>> _aa*_bb
+340
+>>> var=10
+>>> Var=20
+>>> var
+10
+>>> Var
+20
